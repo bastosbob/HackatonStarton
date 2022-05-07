9c39d3514dc21b1af1967007ea245fccd2e0e46e
bastosbob <bastiencantet@outlook.fr>
2022-05-07 18:10:43 +0200

    feat: add base

--- src/App.js
+++ src/App.js
@@ -2,37 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {AppBar, Toolbar, IconButton, Icon, Typography} from "@mui/material";
+import {AppBar, Toolbar, IconButton, Icon, Typography, Rout} from "@mui/material";
 import Button from '@mui/material/Button';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import '@fontsource/roboto/900.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./Home";
+import Form from "./form";
 
 
 
 function App() {
   return (
-
-    <div className="App">
-      <header className="App-header">
-          <link
-              rel="stylesheet"
-              href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-          <AppBar position="sticky">
-              <Toolbar variant="dense">
-                  <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                  </IconButton>
-                  <Typography variant="h6" color="inherit" component="div">
-                      Photos
-                  </Typography>
-              </Toolbar>
-          </AppBar>
-        <Button variant="contained" color="primary">Create a ticket</Button>
-      </header>
-    </div>
+      <BrowserRouter>
+          <Routes>
+              <Route exact path="/" element={<Home/>}/>
+              <Route path="/form" element={<Form/>}/>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import {AppBar, Toolbar, IconButton, Icon, Typography} from "@mui/material";
+import Button from '@mui/material/Button';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/roboto/900.css';
+import { useNavigate} from "react-router-dom";
+
+function Home() {
+    const navigate = useNavigate();
+    return (
+
+        <div className="App">
+            <header className="App-header">
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                />
+                <AppBar>
+                    <Toolbar variant="dense">
+                        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <Button variant="contained" color="primary" onClick={()=>navigate("/form")}>Create a ticket</Button>
+            </header>
+        </div>
+    );
+}
+
+export default Home;
